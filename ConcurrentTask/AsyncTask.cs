34de28fda46ad72d59f7093d9acefa4955ef0d88
shiven143p <shiven143p@gmail.com>
2024-05-27 15:58:17 +0530

    Initial Upload on 27 May 2024
    Initial Upload on 27 May 2024

---
+++ ConcurrentTask/AsyncTask.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Concurrent;
+
+
+namespace ConcurrentTask
+{
+    public static class AsyncTask
+    {
+        public static ConcurrentBag<string> conBag = new();
+
+        public static async Task<List<string>> T1(int n, string taskname)
+        {
+            Console.WriteLine($"Task Execution {taskname}");
+            List<string> stringList = new();
+            try
+            {
+                //throw new Exception("Test Exception: T1");
+                await Task.Delay(1000 * n);
+                stringList = await StringGenerator(n, taskname);
+            }
+            catch (Exception ex)
+            {
+                conBag.Add(taskname + ":" + ex.Message);
+            }
+            return stringList;
+
+        }
+
+        public static async Task<List<string>> T2(int n, string taskname)
+        {
+            Console.WriteLine($"Task Execution {taskname}");
+            List<string> stringList = new();
+            try
+            {
+                await Task.Delay(1000 * n);
+                stringList = await StringGenerator(n, taskname);
+            }
+            catch (Exception ex)
+            {
+                conBag.Add(taskname + ":" + ex.Message);
+            }
+            return stringList;
+        }
+        public static async Task<List<string>> T3(int n, string taskname)
+        {
+            Console.WriteLine($"Task Execution {taskname}");
+            List<string> stringList = new();
+            try
+            {
+                await Task.Delay(1000 * n);
+                stringList = await StringGenerator(n, taskname);
+            }
+            catch (Exception ex)
+            {
+                conBag.Add(taskname + ":" + ex.Message);
+            }
+            return stringList;
+        }
+
+        public static async Task<List<string>> StringGenerator(int n, string taskname)
+        {
+            Console.WriteLine($"StringGenerator: Task Execution {taskname}");
+            List<string> stringList = new() { taskname };
+            object lockCounter = new();
+            try
+            {
+                lock (lockCounter)
+                {
+                    for (int i = 1; i <= n; i++)
+                    {
+                        stringList.Add(Guid.NewGuid().ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                conBag.Add(taskname + ":" + ex.Message);
+            }
+            return await Task.FromResult(stringList);
+        }
+    }
+}
---
+++ ConcurrentTask/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ConcurrentTask
+{
+    class Program
+    {
+        
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Concurrent Async Task Execution!");
+            
+            //Three Task returing genrating n unique string
+            Task<List<string>> Task1 = AsyncTask.T1(4, "T1");
+            Task<List<string>> Task2 = AsyncTask.T2(3, "T2");
+            Task<List<string>> Task3 = AsyncTask.T3(6, "T3");
+
+
+            try
+            {
+                //Concurrent execution of all 3 tasks and their result
+                await Task.WhenAll(Task1, Task2, Task3);
+
+                //Combine result of these tasks in one list
+                List<string> stringList1 = Task1.Result;
+                stringList1.AddRange(Task2.Result);
+                stringList1.AddRange(Task3.Result);
+
+                //Output all result with separator |
+                Console.WriteLine(string.Join('|', stringList1));
+
+
+                //In case any exception it will get also printed
+                //using concurrentbag for thread safe execution
+                foreach (var item in AsyncTask.conBag)
+                {
+                    Console.WriteLine("Exception: " + item.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            
+            //catch (AggregateException ae)
+            //{
+            //    ae.Flatten().Handle(e =>
+            //    {
+            //            Console.WriteLine(e.Message);
+            //            return true;
+            //    });
+            //}
+        }
+
+
+    }
+}
